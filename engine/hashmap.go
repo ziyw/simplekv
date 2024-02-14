3060931ace4ce7845a9b8e4783b7d11e6b7d92be
ziyw <zywong159@gmail.com>
2024-02-14 07:35:27 -0500

    New HashMap done

--- engine/hashmap.go
+++ engine/hashmap.go
@@ -3,50 +3,32 @@ package engine
 import (
 	"bytes"
 	"encoding/gob"
-	"errors"
-	"log"
 )
-
-type hashmap interface {
-	PersistSnapshot(io.write) error
-	LoadSnapshot() error
-}
-
-type offset uint32
 
 // hashmap is key-offset pair. key is a string for the saved key,
 // offset is the offset of the value in segment file.
-type Hashmap map[string]offset
+type offset uint32
+type HashMap map[string]offset
 
-func (h Hashmap) Put(key string, value offset) {
-	h[key] = value
+func NewHashMap() HashMap {
+	return make(map[string]offset)
 }
 
-func (h Hashmap) Get(key string) (offset, error) {
-	v, ok := h[key]
-	if !ok {
-		return 0, errors.New("key does not exist")
-	}
-	return v, nil
-}
-
-// NewSnapshot will encode hashmap to byte array.
-func NewSnapshot(h Hashmap) []byte {
+func Encode(h HashMap) ([]byte, error) {
 	var snapshot bytes.Buffer
-	encode := gob.NewEncoder(&snapshot)
-	if err := encode.Encode(h); err != nil {
-		log.Fatal("error encoding hashmap: %v", err)
+	encoder := gob.NewEncoder(&snapshot)
+	if err := encoder.Encode(h); err != nil {
+		return nil, err
 	}
-	return snapshot.Bytes()
+	return snapshot.Bytes(), nil
 }
 
-// LoadSnapshot
-func LoadSnapshot(snapshot []byte) Hashmap {
+func Decode(snapshot []byte) (HashMap, error) {
 	reader := bytes.NewReader(snapshot)
 	decoder := gob.NewDecoder(reader)
-	h := make(Hashmap)
+	h := make(HashMap)
 	if err := decoder.Decode(&h); err != nil {
-		log.Fatal("error decoding hashmap snapshot: %v", err)
+		return nil, err
 	}
-	return h
+	return h, nil
 }
--- engine/hashmap_test.go
+++ engine/hashmap_test.go
@@ -1,17 +1,49 @@
 package engine
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
-func TestHashmapPut(t *testing.T) {
-	h := NewHashmap()
-	err := h.Put(1, 2)
-	if err != nil {
-		t.Errof("should not got error: %v", err)
+func TestNewHashMap(t *testing.T) {
+	hmap := NewHashMap()
+	if len(hmap) != 0 {
+		t.Error("HashMap init failed")
+	}
+}
+
+func TestPutAndGet(t *testing.T) {
+	hmap := NewHashMap()
+	inputKey, inputValue := "Hello", offset(12)
+	hmap[inputKey] = offset(inputValue)
+	got := hmap[inputKey]
+	fmt.Println(hmap)
+
+	if !reflect.DeepEqual(got, inputValue) {
+		t.Errorf("HashMap put/get wrong: want: %v, got: %v", inputValue, got)
 	}
 
-	// expect error
-	err = h.Put(1, -12)
-	if err == nil {
-		t.Errorf("should got error: %v", err)
+}
+
+func TestHashMapEncodeAndDecode(t *testing.T) {
+	hmap := NewHashMap()
+	hmap["hello"] = 12
+	hmap["world"] = 13
+
+	encoded, _ := Encode(hmap)
+	got, _ := Decode(encoded)
+
+	if !reflect.DeepEqual(hmap, got) {
+		t.Errorf("HashMap decode/encode wrong: want %v, got %v", hmap, got)
+	}
+}
+
+func TestEmptyEncoding(t *testing.T) {
+	hmap := NewHashMap()
+	encoded, _ := Encode(hmap) // encoded empty map is not empty byte array
+	got, _ := Decode(encoded)
+	if !reflect.DeepEqual(hmap, got) {
+		t.Errorf("HashMap decode/encode wrong: want %v, got %v", hmap, got)
 	}
 }
