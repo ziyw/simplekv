d41ec15e84022a6e9ee9789000a53af2ffec8b86
ziyw <zywong159@gmail.com>
2024-02-15 07:17:26 -0500

    Entry func and Append is done

--- engine/seg_file.go
+++ engine/seg_file.go
@@ -2,23 +2,51 @@ package engine
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 )
+
+const (
+	ENTRY_SEPARATOR = ","
+	ENTRY_END       = "\n"
+)
+
+// Create a new string entry from key-value.
+func NewEntry(key, value string) string {
+	if strings.Contains(key, ENTRY_SEPARATOR) || strings.Contains(key, ENTRY_END) {
+		return ""
+	}
+	if strings.Contains(value, ENTRY_SEPARATOR) || strings.Contains(value, ENTRY_END) {
+		return ""
+	}
+
+	return fmt.Sprintf("%s%s%s%s", key, ENTRY_SEPARATOR, value, ENTRY_END)
+}
+
+// Parse a string entry back to key-value pair.
+func ParseEntry(entry string) (string, string, error) {
+	if len(entry) == 0 {
+		return "", "", errors.New("illegal entry: can't be empty")
+	}
+	if !strings.Contains(entry, ENTRY_SEPARATOR) && !strings.Contains(entry, ENTRY_END) {
+		return "", "", errors.New("illegal entry, should contain separators")
+	}
+
+	keyEnd := strings.Index(entry, ENTRY_SEPARATOR)
+	valueEnd := strings.Index(entry, ENTRY_END)
+
+	b := []byte(entry)
+	return string(b[0:keyEnd]), string(b[keyEnd+1 : valueEnd]), nil
+}
 
 type SegFile struct {
 	name string
 }
 
-// Functions need to cover:
-// get(offset), get key-value pair from SegmentFile, offset is error cases
-// Append: append new key-value to SegmentFile, return current SegmentFile offset
-// NewSegmentFile: create a new empty file
-// GetAll(): return all Key-value pair as array, because there will be duplicate Key-value
-// Compress and Merge
-//
-
 func NewSegFile(filename string) (*SegFile, error) {
+	// TODO: maybe when file already exist, use the existing file, no error
 	// file already exist, return nil
 	_, err := os.Stat(filename)
 	if err == nil {
@@ -40,4 +68,25 @@ func (s SegFile) Delete() {
 	if err := os.Remove(s.name); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func (s SegFile) Append(entry string) (Offset, error) {
+	// segment file should exist before append to the file
+	var fileSize Offset = 0
+	info, err := os.Stat(s.name)
+	if err != nil {
+		return 0, err
+	}
+	fileSize = Offset(info.Size())
+
+	f, err := os.OpenFile(s.name, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fileSize, err
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(entry); err != nil {
+		return fileSize, err
+	}
+	return fileSize, nil
 }
--- engine/seg_file_test.go
+++ engine/seg_file_test.go
@@ -1,8 +1,58 @@
 package engine
 
 import (
+	"reflect"
 	"testing"
 )
+
+func TestNewEntry(t *testing.T) {
+	tests := []struct {
+		name       string
+		inputKey   string
+		inputValue string
+		want       string
+	}{
+		{name: "test emtpy input", inputKey: "", inputValue: "", want: ",\n"},
+		{name: "test normal input", inputKey: "Key", inputValue: "Value", want: "Key,Value\n"},
+		{name: "test illegal key", inputKey: "key\n,", inputValue: "value", want: ""},
+		{name: "test illegal value", inputKey: "key", inputValue: "value,\n", want: ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewEntry(tt.inputKey, tt.inputValue)
+			if !reflect.DeepEqual(tt.want, got) {
+				t.Errorf("test %s error, want %v, got %v", tt.name, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestParseNewEntry(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     string
+		wantKey   string
+		wantValue string
+		wantError bool
+	}{
+		{name: "test empty input", input: "", wantKey: "", wantValue: "", wantError: true},
+		{name: "test illegal input", input: "whatever", wantKey: "", wantValue: "", wantError: true},
+		{name: "test normal input", input: "key,value\n", wantKey: "key", wantValue: "value", wantError: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotKey, gotValue, err := ParseEntry(tt.input)
+			if (err != nil) != tt.wantError {
+				t.Errorf("test %s should return error, input: %v", tt.name, tt.input)
+			}
+			if gotKey != tt.wantKey || gotValue != tt.wantValue {
+				t.Errorf("test %s error, want %v %v, got %v %v", tt.name, gotKey, gotValue, tt.wantKey, tt.wantValue)
+			}
+		})
+	}
+}
 
 func TestNewSegmentFile(t *testing.T) {
 	tests := []struct {
@@ -29,3 +79,35 @@ func TestNewSegmentFile(t *testing.T) {
 		})
 	}
 }
+
+func TestAppend(t *testing.T) {
+	// if file doesn't exist before Append, get error
+	badSegFile := SegFile{
+		name: "not_exist",
+	}
+	_, err := badSegFile.Append("helloworld")
+	if err == nil {
+		t.Errorf("when segFile is not created, should return error")
+	}
+
+	// when file already exist, directly append to file
+	goodSegFile, _ := NewSegFile("goodFile")
+	//defer goodSegFile.Delete()
+
+	offset, _ := goodSegFile.Append("HelloWorld")
+	if offset != 0 {
+		t.Errorf("offset is wrong, want %v, got %v", 0, offset)
+	}
+	newOffset, _ := goodSegFile.Append("HelloAgain")
+	if newOffset == 0 {
+		t.Errorf("offset is wrong, new offset should not be zero")
+	}
+}
+
+func TestGet(t *testing.T) {}
+
+func TestGetAll(t *testing.T) {}
+
+func TestCompress(t *testing.T) {}
+
+func TestMerge(t *testing.T) {}
