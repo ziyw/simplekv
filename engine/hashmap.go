38a89caa7f1d914c61db614f8d50d183bb3d9c31
ziyw <zywong159@gmail.com>
2024-03-04 07:30:32 -0500

    Update hashmap

--- engine/hashmap.go
+++ engine/hashmap.go
@@ -3,30 +3,100 @@ package engine
 import (
 	"bytes"
 	"encoding/gob"
+	"errors"
+	"fmt"
 )
 
-// hashmap is key-Offset pair. key is a string for the saved key,
-// Offset is the Offset of the value in segment file.
-type Offset uint32
-type HashMap map[string]Offset
+// HashMap is the in-memory key-offset pair.
+// It includes a in memory map and persist the hashmap content to file.
 
-func NewHashMap() HashMap {
-	return make(map[string]Offset)
+type Offset uint32
+
+type HashMap struct {
+	mem      map[string]Offset
+	filename string
 }
 
-func Encode(h HashMap) ([]byte, error) {
+func NewHashMap(filename string) (*HashMap, error) {
+	if CheckExist(filename) {
+		return nil, errors.New("hashmap file already exist")
+	}
+
+	err := CreateFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return &HashMap{
+		mem:      make(map[string]Offset),
+		filename: filename,
+	}, nil
+}
+
+func LoadHashMap(filename string) (*HashMap, error) {
+	if !CheckExist(filename) {
+		return nil, errors.New("hashmap file does not exist")
+	}
+	hashmap := &HashMap{
+		mem:      make(map[string]Offset),
+		filename: filename,
+	}
+
+	hashmap.Load()
+	return hashmap, nil
+}
+
+// Put and Get, normal hashmap behavior
+func (h *HashMap) Put(key string, value Offset) {
+	h.mem[key] = value
+}
+
+func (h *HashMap) Get(key string) (Offset, error) {
+	if v, ok := h.mem[key]; !ok {
+		return 0, fmt.Errorf("error getting key %v from hashmap", key)
+	} else {
+		return v, nil
+	}
+}
+
+// Persist in-memory map to file.
+func (h *HashMap) Persist() error {
+	encoded, err := encode(h.mem)
+	if err != nil {
+		return err
+	}
+	return WriteFile(h.filename, encoded)
+}
+
+func (h *HashMap) Load() error {
+	content, err := ReadFile(h.filename)
+	if err != nil {
+		return err
+	}
+
+	decoded, err := decode(content)
+	if err != nil {
+		return err
+	}
+	h.mem = decoded
+	return nil
+}
+
+// Encode in-memory map to byte array
+func encode(hashmap map[string]Offset) ([]byte, error) {
 	var snapshot bytes.Buffer
 	encoder := gob.NewEncoder(&snapshot)
-	if err := encoder.Encode(h); err != nil {
+	if err := encoder.Encode(hashmap); err != nil {
 		return nil, err
 	}
 	return snapshot.Bytes(), nil
 }
 
-func Decode(snapshot []byte) (HashMap, error) {
+// Decode byte array to in-memory map
+func decode(snapshot []byte) (map[string]Offset, error) {
 	reader := bytes.NewReader(snapshot)
 	decoder := gob.NewDecoder(reader)
-	h := make(HashMap)
+	h := make(map[string]Offset)
 	if err := decoder.Decode(&h); err != nil {
 		return nil, err
 	}
